00e311ab92a0f7006e3dafc8183c5e29602547be
Islands5 <five.islands.04010613@gmail.com>
2020-12-31 17:07:01 +0900

    hotwire: categoryの一覧、create

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,36 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+    @category = Category.new
+  end
+
+  def create
+    category = Category.create(category_params)
+
+    respond_to do |format|
+      # この辺りの仕様をみると何がしたいかわかる https://turbo.hotwire.dev/handbook/streams
+      format.turbo_stream do
+        render(
+          turbo_stream:
+            turbo_stream.append(
+              :categories,
+              partial: "categories/category",
+              locals: { category: category }
+            )
+        )
+      end
+
+      format.html { redirect_to(action: :index) }
+      # ポスト前と同じアクション(今回はindex)に返すことで、streamが動作しなくても問題ないようにする
+      # (これがstreamが動かなくてもサービスが停止しない理由)
+      # 見た目同じような挙動になるので、networkタブを確認すること
+      # 302になってたらhotwireがうまく動いてない
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
